9424edea4d70a85d7bbeb35fa0464f1b5def5a2d
alinizam <alinizam99@gmail.com>
2017-12-06 14:41:34 +0200

    Giriş Dersi

---
+++ src/Ders11Lab/Unvan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ders11Lab;
+
+/**
+ *
+ * @author anizam
+ */
+public class Unvan {
+    int unvanId;
+    String adi;
+
+    public Unvan(int unvanId, String adi) {
+        this.unvanId = unvanId;
+        this.adi = adi;
+      
+    }
+
+    @Override
+    public String toString() {
+        return adi; //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+    
+}
